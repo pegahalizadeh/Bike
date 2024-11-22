d364cae9eda3b1c2109f978792ae4dde3e44d241
pegah alizadeh <pegah.alizad@gmail.com>
2024-11-22 19:22:56 +0330

    Bike

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+
+/** @type {import(‘tailwindcss’).Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+  extend: {
+    backgroundImage: {
+      'topBg': "url('../img/hero-1-scaled.jpg')",
+      'topBg2': "url('../img/info-7.jpg')",
+    },
+  
+    
+    
+  
+    
+  },
+  },
+  plugins: [],
+  }
+
+
